8090580170f899314eeb874a84412c0ad782556e
Simon Solders <simon.solders@gmail.com>
2023-01-03 12:41:36 +0100

    Base setup, plugin file, readme, ignore and changelog

---
+++ ledyer-order-management-for-woocommerce.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name: Ledyer Order Management for WooCommerce
+ * Plugin URI: https://github.com/ledyer/ledyer-order-management-for-woocommerce
+ * Description: Ledyer Order Management for WooCommerce.
+ * Author: Ledyer AB
+ * Author URI: https://www.ledyer.com
+ * Version: 1.0.0
+ * Text Domain: ledyer-order-management-for-woocommerce
+ * Domain Path: /languages
+ *
+ * WC requires at least: 3.2.0
+ * WC tested up to: 4.7.0
+ *
+ * Copyright (c) 2017-2023 Ledyer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// use Ledyer\Ledyer_Order_Management_For_WooCommerce;
+
+\defined( 'ABSPATH' ) || die();
+
+// require_once __DIR__ . '/classes/class-ledyer-om-singleton.php';
+// require_once __DIR__ . '/classes/class-ledyer-om-main.php';
+
+/**
+ * Required minimums and constants
+ */
+// \define( 'LOM_WC_VERSION', Ledyer_Order_Management_For_WooCommerce::VERSION );
+// \define( 'LOM_WC_MIN_PHP_VER', '5.6.0' );
+// \define( 'LOM_WC_MIN_WC_VER', '3.9.0' );
+// \define( 'LOM_WC_MAIN_FILE', __FILE__ );
+// \define( 'LOM_WC_PLUGIN_NAME', dirname( plugin_basename( LOM_WC_MAIN_FILE ) ) );
+// \define( 'LOM_WC_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+// \define( 'LOM_WC_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+function ledyerOm() {
+  echo 'HELLO LEDYER OM';
+	// return Ledyer_Order_Management_For_WooCommerce::instance();
+}
+
+ledyerOm();
