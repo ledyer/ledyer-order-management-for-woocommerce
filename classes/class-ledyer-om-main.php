7a53c8bdd86aecf435bcd63b5cdc841188851bb7
Simon Solders <simon.solders@gmail.com>
2023-01-08 11:17:16 +0100

    cleanup

--- classes/class-ledyer-om-main.php
+++ classes/class-ledyer-om-main.php
@@ -53,36 +53,31 @@ class Ledyer_Order_Management_For_WooCommerce {
 		// add_action( 'schedule_process_notification', array( $this, 'process_notification' ), 10, 1 );
 	}
 
-			/**
-		 * Adds plugin action link to Krokedil documentation for KOM.
-		 *
-		 * @param array $links Plugin action link before filtering.
-		 *
-		 * @return array Filtered links.
-		 */
-		public function plugin_action_links( $links ) {
+	/**
+	 * Adds plugin action link to Krokedil documentation for KOM.
+	 *
+   * @param array $links Plugin action link before filtering.
+	 *
+	 * @return array Filtered links.
+	 */
+	public function plugin_action_links( $links ) {
+		$setting_link = $this->get_setting_link();
+		$plugin_links = array(
+			'<a href="' . $setting_link . '">' . __( 'Settings', 'ledyer-order-management-for-woocommerce' ) . '</a>',
+			'<a target="_blank" href="https://ledyer.com">Docs</a>',
+		);
 
-			$setting_link = $this->get_setting_link();
+		return array_merge( $plugin_links, $links );
+	}
 
-			$plugin_links = array(
-				'<a href="' . $setting_link . '">' . __( 'Settings', 'ledyer-order-management-for-woocommerce' ) . '</a>',
-				'<a target="_blank" href="https://ledyer.com">Docs</a>',
-			);
-
-			return array_merge( $plugin_links, $links );
-		}
-
-		/**
-		 * Return the proper link for the settings page of KOM.
-		 *
-		 * @return string The full settings page URL.
-		 */
-		protected function get_setting_link() {
-			return admin_url( 'admin.php?page=lom-settings' );
-		}
-
-
-
+	/**
+	 * Return the proper link for the settings page of KOM.
+	 *
+	 * @return string The full settings page URL.
+	 */
+	protected function get_setting_link() {
+		return admin_url( 'admin.php?page=lom-settings' );
+	}
 
 	public function on_plugins_loaded() {
 		if ( ! defined( 'WC_VERSION' ) ) {
@@ -107,7 +102,7 @@ class Ledyer_Order_Management_For_WooCommerce {
 		));
 	}
 
-		/**
+	/**
 	 * Init meta box on admin hook.
 	 */
 	public function on_admin_init() {
@@ -116,29 +111,6 @@ class Ledyer_Order_Management_For_WooCommerce {
 
 	public function include_files() {
 		include_once LOM_WC_PLUGIN_PATH . '/includes/lom-functions.php';
-		// include_once LCO_WC_PLUGIN_PATH . '/includes/lco-types.php';
 		include_once LOM_WC_PLUGIN_PATH . '/classes/class-ledyer-om-settings.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-logger.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/admin/class-ledyer-meta-box.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/class-ledyer-request.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/class-ledyer-request-order.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/session/class-ledyer-create-order.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/session/class-ledyer-get-order.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/session/class-ledyer-update-order.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/management/class-ledyer-acknowledge-order.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/management/class-ledyer-get-order.php';
-		// include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/management/class-ledyer-get-payment-status.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/order/management/class-ledyer-update-order-reference.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-ledyer-cart.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-ledyer-woocommerce-bridge.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-ajax.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-api.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-checkout.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-confirmation.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-credentials.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-fields.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-lco-gateway.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-merchant-urls.php';
-    //     include_once LCO_WC_PLUGIN_PATH . '/classes/class-ledyer-templates.php';
 	}
 }
