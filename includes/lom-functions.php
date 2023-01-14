4e3615614f0959d3ed642f66e2aa7d920bcb1a2e
Simon Solders <simon.solders@gmail.com>
2023-01-14 14:02:49 +0100

    removed dummy function

--- includes/lom-functions.php
+++ includes/lom-functions.php
@@ -7,17 +7,6 @@
 
 \defined( 'ABSPATH' ) || die();
 
-/**
- * Dummy
- *
- * @return boolean
- */
-function lom_hello_world() {
-	echo 'hello world from functions';
-
-	return false;
-}
-
 function order_placed_with_ledyer($payment_method) {
 	if ( in_array($payment_method, array('ledyer_payments', 'lco')) ) {
 		return true;
