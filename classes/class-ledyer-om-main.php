218752bf92b79d882b59d90da96223c1b8f6cb4f
Peter Larsson <peter@pln.se>
2023-03-03 12:06:37 +0100

    fix: version bump (#16)

--- classes/class-ledyer-om-main.php
+++ classes/class-ledyer-om-main.php
@@ -18,7 +18,7 @@ class Ledyer_Order_Management_For_WooCommerce {
 	public $parentSettings;
 	public $api;
 
-	const VERSION = '1.2.0';
+	const VERSION = '1.3.0';
 	const SLUG = 'ledyer-order-management-for-woocommerce';
 	const SETTINGS = 'ledyer_order_management_for_woocommerce_settings';
 
