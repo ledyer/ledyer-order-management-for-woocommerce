7801e38c7a36e4d89d714fbf87a0799b79e052d3
Simon Solders <simon.solders@gmail.com>
2023-05-05 14:24:05 +0200

    Fix/wp review updates (#21)

--- ledyer-order-management-for-woocommerce.php
+++ ledyer-order-management-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Ledyer Order Management for WooCommerce.
  * Author: Ledyer AB
  * Author URI: https://www.ledyer.com
- * Version: 1.4.1
+ * Version: 1.4.2
  * Text Domain: ledyer-order-management-for-woocommerce
  * Domain Path: /languages
  *
--- readme.txt
+++ readme.txt
@@ -7,6 +7,6 @@ Tested up to: 6.2
 Requires PHP: 7.0
 WC requires at least: 4.0.0
 WC tested up to: 6.9.3
-Stable tag: 1.4.1
+Stable tag: 1.4.2
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
