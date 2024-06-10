2d1cc594b72a15d8bd95a56e4fc3ec8b96346ba5
Niklas Högefjord <niklas@krokedil.se>
2024-06-10 16:27:14 +0200

    PHPCS

--- classes/class-ledyer-om-main.php
+++ classes/class-ledyer-om-main.php
@@ -8,7 +8,6 @@
 
 namespace LedyerOm;
 
-
 \defined( 'ABSPATH' ) || die();
 
 class Ledyer_Order_Management_For_WooCommerce {
@@ -18,16 +17,17 @@ class Ledyer_Order_Management_For_WooCommerce {
 	public $parentSettings;
 	public $api;
 
-	const VERSION = '1.5.0';
-	const SLUG = 'ledyer-order-management-for-woocommerce';
+	const VERSION  = '1.5.0';
+	const SLUG     = 'ledyer-order-management-for-woocommerce';
 	const SETTINGS = 'ledyer_order_management_for_woocommerce_settings';
 
 	/**
 	 * Summary of actions - called from class-ledyer-om-singleton.php
+	 *
 	 * @return void
 	 */
 	public function actions() {
-		add_action( 'plugins_loaded', [ $this, 'on_plugins_loaded' ] );
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
 
 		// Add refunds support to Ledyer Payments and Ledyer Checkout gateways.
 		add_action( 'wc_ledyer_payments_supports', array( $this, 'add_gateway_support' ) );
@@ -36,60 +36,66 @@ class Ledyer_Order_Management_For_WooCommerce {
 		// Capture an order -> lom-capture.php
 		add_action(
 			'woocommerce_order_status_completed',
-			function ($order_id, $action = false) {
-				lom_capture_ledyer_order($order_id, $action, $this->api);
+			function ( $order_id, $action = false ) {
+				lom_capture_ledyer_order( $order_id, $action, $this->api );
 			}
 		);
 
 		// Listen to refund from Ledyer Checkout for Woocommerce, then call lom_refund_ledyer_order -> lom-refund.php
 		add_filter(
 			'wc_ledyer_checkout_process_refund',
-			function ($result, $order_id, $amount, $reason) {
-				return lom_refund_ledyer_order($order_id, $amount, $this->api);
+			function ( $result, $order_id, $amount, $reason ) {
+				return lom_refund_ledyer_order( $order_id, $amount, $this->api );
 			},
-			10, 4);
+			10,
+			4
+		);
 
 		// Listen to refund from Ledyer Payments for Woocommerce, then call lom_refund_ledyer_order -> lom-refund.php
 		add_filter(
 			'wc_ledyer_payments_process_refund',
-			function ($result, $order_id, $amount, $reason) {
-				return lom_refund_ledyer_order($order_id, $amount, $this->api);
+			function ( $result, $order_id, $amount, $reason ) {
+				return lom_refund_ledyer_order( $order_id, $amount, $this->api );
 			},
-			10, 4);
+			10,
+			4
+		);
 
 		// Cancel an order -> lom-cancel.php
 		add_action(
 			'woocommerce_order_status_cancelled',
-			function ($order_id, $action = false) {
-				lom_cancel_ledyer_order($order_id, $action, $this->api);
+			function ( $order_id, $action = false ) {
+				lom_cancel_ledyer_order( $order_id, $action, $this->api );
 			}
 		);
 
 		// Sync order items and totals
 		add_action(
 			'woocommerce_saved_order_items',
-			function ($order_id, $action = false) {
-				lom_edit_ledyer_order($order_id, $action, $this->api, "order");
+			function ( $order_id, $action = false ) {
+				lom_edit_ledyer_order( $order_id, $action, $this->api, 'order' );
 			}
 		);
 
 		// Sync customer details such as shipping and billing
 		add_action(
 			'woocommerce_process_shop_order_meta',
-			function ($order_id, $action = false) {
+			function ( $order_id, $action = false ) {
 
-				if (!is_admin()) {
+				if ( ! is_admin() ) {
 					return;
 				}
-				lom_edit_ledyer_order($order_id, $action, $this->api, "customer");
-			}, 55, 1 // Priority higher than 50, since that is the priority we use to save the fields after validation is done. We don't want to sync with Ledyer if local Woo validation of the fields fail.
+				lom_edit_ledyer_order( $order_id, $action, $this->api, 'customer' );
+			},
+			55,
+			1 // Priority higher than 50, since that is the priority we use to save the fields after validation is done. We don't want to sync with Ledyer if local Woo validation of the fields fail.
 		);
 	}
 
 	/**
 	 * Adds plugin action link to Ledyer documentation for LOM.
 	 *
-   * @param array $links Plugin action link before filtering.
+	 * @param array $links Plugin action link before filtering.
 	 *
 	 * @return array Filtered links.
 	 */
@@ -122,11 +128,11 @@ class Ledyer_Order_Management_For_WooCommerce {
 		$this->include_files();
 		$this->set_settings();
 
-		$this->credentials      = Credentials::instance();
-		$this->parentSettings   = ParentSettings::instance();
-		$this->api              = new API();
+		$this->credentials    = Credentials::instance();
+		$this->parentSettings = ParentSettings::instance();
+		$this->api            = new API();
 
-		add_filter( 'plugin_action_links_' . plugin_basename( LOM_WC_MAIN_FILE ), array($this, 'plugin_action_links'));
+		add_filter( 'plugin_action_links_' . plugin_basename( LOM_WC_MAIN_FILE ), array( $this, 'plugin_action_links' ) );
 	}
 
 	/**
@@ -173,7 +179,5 @@ class Ledyer_Order_Management_For_WooCommerce {
 		include_once LOM_WC_PLUGIN_PATH . '/classes/requests/order/class-ledyer-om-request-cancel-order.php';
 		include_once LOM_WC_PLUGIN_PATH . '/classes/requests/order/class-ledyer-om-request-edit-order.php';
 		include_once LOM_WC_PLUGIN_PATH . '/classes/requests/order/class-ledyer-om-request-edit-customer.php';
-
-
 	}
 }
